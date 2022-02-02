ecbe72141688561b3bd08baedc356633031f2d7a
yantis <yantis1212@outlook.com>
2022-02-02 07:32:49 -0700

    feat
    Add node server for practice in routing

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 /////////////////////////////////////////
 //FILES
@@ -33,7 +34,19 @@ const http = require('http');
 
 // SERVER
 const server = http.createServer((req, res) => {
-	res.end('Hello from the server!');
+	const pathName = req.url;
+	console.log(pathName);
+	if (pathName === '/' || pathName === '/overview') {
+		res.end('This is the overview');
+	} else if (pathName === '/product') {
+		res.end('This is product');
+	} else {
+		res.writeHead(404, {
+			'Content-type': 'text/html',
+			'my-own-header': 'hello-world',
+		});
+		res.end('<h1>Page not found.</h1>');
+	}
 });
 
 server.listen(8000, '127.0.0.1', () => {
